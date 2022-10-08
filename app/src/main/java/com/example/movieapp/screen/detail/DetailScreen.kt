605fbea61a12ee2b449df060206615ae0ba151a8
Awais Ahmad <awais.ahmad@venturedive.com>
2022-10-08 12:34:27 +0500

    Movie Detailed Screen

--- app/src/main/java/com/example/movieapp/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/example/movieapp/screen/detail/DetailScreen.kt
@@ -1,7 +1,10 @@
 package com.example.movieapp.screen.detail
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -11,9 +14,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.rememberImagePainter
+import com.example.movieapp.model.getMovies
+import com.example.movieapp.widgets.MovieRow
 
 @Composable
-fun DetailScreen(navController: NavHostController, movieName: String?) {
+fun DetailScreen(navController: NavHostController, movieId: String?) {
+    val movie = getMovies().filter { it.id == movieId }.first()
     Scaffold(topBar = {
         TopAppBar(
             backgroundColor = Color.Transparent,
@@ -25,7 +32,7 @@ fun DetailScreen(navController: NavHostController, movieName: String?) {
                         navController.popBackStack()
                     })
                 Spacer(modifier = Modifier.width(100.dp))
-                Text(text = movieName.toString())
+                Text(text = movie.title)
             }
         }
     }) {
@@ -36,9 +43,27 @@ fun DetailScreen(navController: NavHostController, movieName: String?) {
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
-                Text(text = movieName.toString(), style = MaterialTheme.typography.h5)
+                MovieRow(movie = movie)
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                LazyRow {
+                    items(movie.images) {
+                        Card(
+                            modifier = Modifier
+                                .padding(12.dp)
+                                .size(240.dp),
+                            elevation = 5.dp
+                        ) {
+                            Image(
+                                painter = rememberImagePainter(data = it),
+                                contentDescription = "Movie Poster"
+                            )
+                        }
+                    }
+                }
             }
         }
     }
